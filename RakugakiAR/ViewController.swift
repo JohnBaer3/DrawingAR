a2c7de763d9a07a704c5e98d54f85112072817d5
John Baer <johnbaer3@gmail.com>
2020-09-11 17:45:02 -0700

    Initial Commit

---
+++ RakugakiAR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RakugakiAR
+//
+//  Created by John Baer on 9/11/20.
+//  Copyright © 2020 John Baer. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
